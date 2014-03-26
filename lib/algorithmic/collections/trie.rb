bb8d0b9c4e6d3eb0851f2b7d9cd70a1586eda784
kristenmills <kristen@kristen-mills.com>
2014-03-26 18:58:44 -0400

    More trie

--- lib/algorithmic/collections/trie.rb
+++ lib/algorithmic/collections/trie.rb
@@ -6,15 +6,22 @@ module Collections
     attr_accessor :compressed
 
     # Constructor for the Trie
-    def initialize
-      @root = TrieNode.new
+    #
+    # @param [Object] default the default value for the trie
+    def initialize(default)
+      @default = default
+      @root = TrieNode.new(default)
       @compressed = false
     end
 
-    def add(string, value)
+    # Adds a new key value pair to the trie
+    #
+    # @param [String] key the key
+    # @Param [Object] value the value to be stored at the key
+    def add(key, value)
 			node = @root
-			string.each_char do |char|
-				node.children[char] ||= TrieNode.new
+			key.each_byte do |char|
+				node.children[char-65] ||= TrieNode.new(@default_value)
 				node = node.walk(char)
 			end
       node.value = value
@@ -23,6 +30,46 @@ module Collections
 
     alias_method :[]=, :add
 
+    # Delete a key from the the trie
+    #
+    # @param [String] key the string to delete
+    # @return [Object] the value at the key or default value if it isn't there
+    def delete(key)
+      node = @root
+			word.each_byte do |c|
+				return @default_value unless node = node.walk(c)
+			end
+			node.terminal = false
+      val = node.value
+      node.value = @default_value
+      val
+    end
+
+    # Get the value at the given key
+    #
+    # @param [String] key the key you are trying to access
+    # @return [Object] the value at the given key
+    def get(key)
+      node = @root
+      word.each_byte do |c|
+        return @default_value unless node = node.walk(c)
+      end
+      node.value
+    end
+
+    alias_method :[], :get
+
+    # Does the trie have the given key
+    #
+    # @param [String] key the key you are searching for
+    # @return true if it has the key false if it doesn't
+    def has_key?(key)
+      node = @root
+      word.each_byte do |c|
+        return false unless node = node.walk(c)
+      end
+      node.terminal? ? true : false
+    end
   end
 
   # A Node for a Trie
@@ -33,7 +80,7 @@ module Collections
     #
     # @param [Object] value the value to store at this trie
     def initialize(value=nil)
-      @children = Hash.new
+      @children = Array.new(26)
       @value = value
       @terminal = false
     end
@@ -43,7 +90,7 @@ module Collections
 		# @param [String] char the character you are trying to reach
 		# @return the node for that char or nil if it doesn't exist
     def walk(char)
-      @children[char]
+      @children[char-65]
     end
 
     # Is this node a leaf node
